cc4a23b26fe6e59fd7d431988c8f86f0f0b96a95
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-10 09:07:16 +0200

    feat: add constants declaration

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// #region Constants declaration section
+
+    // region Manual parameters constants
+
+        // Minumum random number to generate
+        const MIN_RANDOM_NUMBER = 1;
+
+        // Maximum random number to generate
+        const MAX_RANDOM_NUMBER = 50;
+
+        // Total of random numbers
+        const TOTAL_NUMBERS = 5;
+
+        // Available seconds to the user for memorizing random numbers
+        const AVAILABLE_SECONDS = 3
+
+    // endregion Manual parameters constants
+
+    // #region DOM elements
+
+        // Countdown DOM element
+        const countdownElement = document.getElementById('countdown');
+
+        // Instructions DOM element
+        const instructionsElement = document.getElementById('instructions');
+
+        // Numbers list DOM element
+        const numberListElement = document.getElementById('numbers-list');
+
+        // Answers form DOM element
+        const answersFormElement = document.getElementById('answers-form');
+
+        // Message DOM element
+        const messageElement = document.getElementById('message');
+
+    // #endregion DOM elements
+
+// #endregion Constants declaration section
